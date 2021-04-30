15cfe28cb16bbbcfb3c48d7b2eb025c81c6e7cb0
Jhonny Lanzuisi <jalb97@gmail.com>
2021-04-30 10:44:58 -0400

    Clean up and split bib files

---
+++ bib/auto/general.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "general"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "noauthor_caribbean_2021"
+    "waite1966return"
+    "senior_arrival_2009"
+    "walcott_africa"
+    "noauthor_litcharts_nodate"
+    "noauthor_saint_2021"
+    "kaldewaij_programming:_1992"
+    "mezaravelo_programacion"
+    "noauthor_apartheid_2021"
+    "noauthor_nadine_2021"
+    "nadine_dreaming"
+    "khushwant_singh_train_1990"
+    "latinModernFonts_website"
+    "bringhurst_elements_2004"
+    "noauthor_buttericks_nodate"
+    "morison_tally_1973"
+    "van_dongen_latex_2012"))
+ :bibtex)
+
